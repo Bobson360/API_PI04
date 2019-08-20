db67520e4e99f08d44a2d4f5c6bd22ccad7394c9
Robson Rodrigues Da Silva <robson.silva@inmetrics.com.br>
2019-08-20 17:40:44 -0300

    Rota nova categoria criada

--- src/main/java/com/robson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/robson/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.robson.cursomc.resources;
 
+import java.net.URI;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.robson.cursomc.domain.Categoria;
 import com.robson.cursomc.services.CategoriaService;
@@ -22,16 +27,12 @@ public class CategoriaResource {
 		Categoria obj = service.bucar(id);
 		
 		return ResponseEntity.ok().body(obj);
-		
-//		Categoria cat1 = new Categoria(1, "Informatica");
-//		Categoria cat2 = new Categoria(2, "Escritório");
-//		
-//		List<Categoria> lista = new ArrayList<>();
-//		
-//		lista.add(cat1);
-//		lista.add(cat2);
-//		
-//		return lista;
-		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
