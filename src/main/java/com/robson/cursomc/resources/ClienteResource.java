36de59b3affe3ae9e726ce095c4549b6a0086d3b
Robson Rodrigues Da Silva <robson.silva@inmetrics.com.br>
2019-08-20 18:05:43 -0300

    Atualizando categoria com PUT, ajustes em nomes de metodos

--- src/main/java/com/robson/cursomc/resources/ClienteResource.java
+++ src/main/java/com/robson/cursomc/resources/ClienteResource.java
@@ -1,11 +1,15 @@
 package com.robson.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.robson.cursomc.domain.Cliente;
 import com.robson.cursomc.services.ClienteService;
@@ -18,10 +22,15 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.bucar(id);
-		
 		return ResponseEntity.ok().body(obj);
-		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Cliente obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
