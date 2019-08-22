a1d5b00ffb9c8e2918166d7401806a66b16955f3
Robson Rodrigues Da Silva <robson.silva@inmetrics.com.br>
2019-08-22 18:35:36 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/robson/cursomc/services/CategoriaService.java
+++ src/main/java/com/robson/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.robson.cursomc.domain.Categoria;
+import com.robson.cursomc.dto.CategoriaDTO;
 import com.robson.cursomc.repositories.CategoriaRepository;
 import com.robson.cursomc.services.exeptions.DataIntegrityException;
 import com.robson.cursomc.services.exeptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
